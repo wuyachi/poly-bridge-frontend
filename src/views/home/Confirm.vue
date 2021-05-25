1173bf315afa8be9c24e12767d77a72666b8b6ba
LinK <122826933@qq.com>
2021-05-25 16:38:35 +0800

    update confirm receive token name

--- src/views/home/Confirm.vue
+++ src/views/home/Confirm.vue
@@ -68,7 +68,7 @@
               <div class="label">{{ $t('home.confirm.receiving') }}</div>
               <div class="fee">
                 <span class="fee-value">{{ $formatNumber(receivingAmount) }}</span>
-                <span class="token-basic-name">{{ toToken.name }}</span>
+                <span class="token-basic-name">{{ toToken.tokenBasicName }}</span>
               </div>
             </div>
           </div>
