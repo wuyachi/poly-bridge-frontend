c39f4df256cba275ca102beca1a38d4ffb317cf4
LinK <122826933@qq.com>
2021-06-01 14:55:09 +0800

    update max available amount

--- src/i18n/zh-cn.js
+++ src/i18n/zh-cn.js
@@ -48,8 +48,8 @@ export default {
       connectWallet: 'Connect Wallet',
       historyPrefix: 'You can view your',
       historyLink: 'history',
-      maxamount: "Max Amount",
-      maxamountTooltip: "The largest number of cross-chain assets on the currently selected target chain (estimated value)"
+      maxamount: "Max Available Amount",
+      maxamountTooltip: "The largest available number of cross-chain assets on the currently selected target chain (estimated value)"
     },
     selectTokenBasic: {
       title: 'Select Asset',
